45e4b66409dfc2a54a45c5a68e17bb7535577219
Fred Morrison <frederick.morrison@outlook.com>
2025-02-06 16:27:08 -0700

    Change StringBuilder to inherit from io.StringIO

--- string_builder.py
+++ string_builder.py
@@ -1,15 +1,19 @@
 from io import StringIO
 
-class StringBuilder():
+
+class StringBuilder(StringIO):
+    """ Implements a better StringIO API without all the flaws of StringIO """
+
 
     def __init__(self, initial_value = ''):
-        self.value = initial_value
-        self.buffer = StringIO(initial_value)
+        super().__init__(initial_value)
         # HERE IS WHERE WE OVERCOME THE DESIGN FLAW IN STRINGIO:
-        self.buffer.seek(len(initial_value))
+        super().seek(len(initial_value))
+
 
     def append(self, new_value: str) -> None:
-        self.buffer.write(new_value)
+        super().write(new_value)
+
 
     def __str__(self) -> str:
-        return self.buffer.getvalue()
+        return super().getvalue()
