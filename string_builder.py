95ba361cbb98d686d5b754d74e9eea0744eb2c52
Fred Morrison <frederick.morrison@outlook.com>
2025-02-06 16:30:33 -0700

    Change StringBuilder to inherit from io.StringIO

--- string_builder.py
+++ string_builder.py
@@ -6,12 +6,14 @@ class StringBuilder(StringIO):
 
 
     def __init__(self, initial_value = ''):
+        """ Initialize an instance of the class without the flaw of the underlying parent class, StringIO"""
         super().__init__(initial_value)
         # HERE IS WHERE WE OVERCOME THE DESIGN FLAW IN STRINGIO:
         super().seek(len(initial_value))
 
 
     def append(self, new_value: str) -> None:
+        """ append the specified string to the end of the buffer."""
         super().write(new_value)
 
 
