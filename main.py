739a9b90ab0126c6d0158403caec636f7380bfc0
Fred Morrison <frederick.morrison@outlook.com>
2025-02-06 21:30:23 -0700

    add index-based insert capability to StringBuilder

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 from string_builder import StringBuilder
 
+
 def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
+
 
 def main():
     sbr = StringBuilder('Start ')
@@ -10,6 +12,11 @@ def main():
     sbr.append('B ')
     sbr.append('C')
     print(sbr)
+
+    # try inserting half way into the buffer
+    sbr.insert(sbr.tell() // 2, 'inserted')
+    print(sbr)
+
 
 if __name__ == '__main__':
     print(f'Python version: {get_python_version()}')
--- string_builder.py
+++ string_builder.py
@@ -9,12 +9,22 @@ class StringBuilder(StringIO):
         """ Initialize an instance of the class without the flaw of the underlying parent class, StringIO"""
         super().__init__(initial_value)
         # HERE IS WHERE WE OVERCOME THE DESIGN FLAW IN STRINGIO:
-        super().seek(len(initial_value))
+        super().seek(len(super().getvalue()))
 
 
     def append(self, new_value: str) -> None:
         """ append the specified string to the end of the buffer."""
         super().write(new_value)
+
+
+    def insert(self, index: int, new_value: str) -> None:
+        """ insert the specified string at the specified index within the buffer."""
+        super().seek(0)
+        existing_value = super().getvalue()
+        modified_value = existing_value[:index] + new_value + existing_value[index:]
+        super().seek(0)
+        super().truncate(0)
+        super().write(modified_value)
 
 
     def __str__(self) -> str:
